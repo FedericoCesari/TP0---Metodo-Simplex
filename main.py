d905ac84ad3f3f6ec00b28c9e44d3d1657ff5ab6
AxMar01 <martinoaxel01@gmail.com>
2024-11-03 21:05:56 -0300

    Actualización de main
    Se divide el main en funciones para poder hacer el call del menu principal despues del except dado por caracteres no numericos en los archivos

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ import matricessalida
 import archivos2
 import numpy as np
 
-print("Bienvenido al sistema de resolución SIMPLEX\n")
-print("Para empezar, elija una de las siguientes opciones:\n")
-print("[1] Carga manual")
-print("[2] Carga por archivo")
-version_simplex=0
+def menu_principal():
+    print("Elija una de las siguientes opciones:\n")
+    print("[1] Carga manual")
+    print("[2] Carga por archivo")
+    version_simplex=0
 
-while version_simplex not in (1, 2):
-    try:
-        version_simplex = int(input("Ingrese una opción: "))
-        if version_simplex not in (1, 2):
-            print("Valor no válido, vuelva a intentarlo.")
-    except ValueError:
-        print("Entrada inválida. Por favor, ingrese un número entero.")
-    
-if version_simplex == 1:
-
+    while version_simplex not in (1, 2):
+        try:
+            version_simplex = int(input("Ingrese una opción: "))
+            if version_simplex not in (1, 2):
+                print("Valor no válido, vuelva a intentarlo.")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingrese un número entero.")
+            
+        if version_simplex == 1:
+            ingreso_manual()
+        elif version_simplex==2:
+            carga_por_archivo()
+            
+def ingreso_manual():
     num_variables = int(input("Ingrese numero de variables: "))
     num_restricciones = int(input("Ingrese numero de restricciones: "))
     print("")
@@ -44,7 +48,7 @@ if version_simplex == 1:
     print("Matrices guardadas en 'salidas.csv'")
 
 
-elif version_simplex == 2:
+def carga_por_archivo():
     directorio = "Archivos Matrices"
 
     lista_archivos = archivos2.listar_archivos_txt(directorio)
@@ -66,5 +70,8 @@ elif version_simplex == 2:
         matricessalida.save_matrices_to_csv(matricessalida.matrices_generadas, 'salidas.csv')
         print("Matrices guardadas en 'salidas.csv'")
     else:
-        print("El archivo ingresado contiene valores no numéricos, verifique el archivo o intentelo con uno diferente")
+        print("\nEl archivo ingresado contiene valores no numéricos, verifique el archivo o intentelo con uno diferente\n")
+        menu_principal()
        
+print("Bienvenido al sistema de resolución SIMPLEX\n")
+menu_principal()
